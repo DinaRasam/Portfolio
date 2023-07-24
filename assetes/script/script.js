cc49d30037a406c2a76c31e5f2377c4f55e5a915
DinaRasam <dinarasam10@gmail.com>
2023-07-24 18:41:24 +0200

    push

---
+++ assetes/script/script.js
@@ -0,0 +1,45 @@
+// alert("mety")
+
+let bouton = document.getElementById("darck")
+bouton.addEventListener('click',()=>{
+    console.log("bouton cliker")
+
+    let teste = document.getElementById("about-me")
+    console.log("mety ve");
+
+    teste.style.backgroundColor = "black"
+    let teste2 = document.getElementsByClassName("titre2")
+    teste2.style.color = "white"
+});
+
+let Dina={
+    About:[
+        {
+        nom : "ANDRIAMIFIDISAMIMANANA",
+        Prenom : "Dinaniaina",
+        telephone : "+261 30 565 38",
+        mail : "dinarasam10@gmail.com",
+        adress : "Faravohitra Avaratra"
+    }
+    ],
+    Skills:[
+        {
+
+    }
+
+    ],
+    Contacts:[
+        {
+       
+    }
+
+    ]
+
+}
+// let descrit1=document.getElementById("descri1")
+// console.log("teste333")
+// descrit1.innerHTML= "<div class=titre1>"+
+// I + "'" + m +"<em>" +Dina +"</em>"+" "+ Rasamimanana+"<br>"+
+// Web+" "+developer +
+// "</div>"+"<br>"
+    
